a7cd10e7276a0c3a5357eedbebbd514b628c9360
rkyzk <reikoyazaki@yahoo.com>
2024-10-18 21:41:20 +0900

    add updateProduct in ProdController

--- src/main/java/com/restapi/ecommerce/controller/ProductController.java
+++ src/main/java/com/restapi/ecommerce/controller/ProductController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,9 +28,16 @@ public class ProductController {
 		return new ResponseEntity<> (prodList, HttpStatus.OK);
 	}
 	
-	@PostMapping("/public/products")
+	@PostMapping("/admin/products")
 	public ResponseEntity<Product> postProduct(@RequestBody Product product) {
 		Product savedProduct = productService.createProduct(product);
 		return new ResponseEntity<> (savedProduct, HttpStatus.CREATED);
+	}
+	
+	@PutMapping("/admin/products/{prodId}")
+	public ResponseEntity<Product> updateProduct(@RequestBody Product product,
+			@PathVariable Long prodId) {
+		Product updatedProduct = productService.updateProduct(product, prodId);
+		return new ResponseEntity<> (updatedProduct, HttpStatus.OK);
 	}
 }
