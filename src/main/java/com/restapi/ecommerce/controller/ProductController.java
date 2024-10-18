4a4a7a7d3a7f6d17307357e23d413af6a5f13dae
rkyzk <reikoyazaki@yahoo.com>
2024-10-18 21:10:53 +0900

    add getProducts method in productController

--- src/main/java/com/restapi/ecommerce/controller/ProductController.java
+++ src/main/java/com/restapi/ecommerce/controller/ProductController.java
@@ -1,13 +1,22 @@
 package com.restapi.ecommerce.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.restapi.ecommerce.entity.Product;
+
 @RestController
+@RequestMapping("/api")
 public class ProductController {
 	
-	@GetMapping("/test")
-	public String getProducts() {
-		return "Hello";
+	@GetMapping("/public/products")
+	public ResponseEntity<List<Product>> getProducts() {
+		List<Product> prodList = productService.getProducts();
+		return new ResponseEntity<> (prodList, HttpStatus.OK);
 	}
 }
