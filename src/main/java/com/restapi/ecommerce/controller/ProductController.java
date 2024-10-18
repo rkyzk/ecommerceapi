5d6da60c588dbd9297804ad2c2cf039774233299
rkyzk <reikoyazaki@yahoo.com>
2024-10-18 21:29:16 +0900

    add postProduct in ProdController

--- src/main/java/com/restapi/ecommerce/controller/ProductController.java
+++ src/main/java/com/restapi/ecommerce/controller/ProductController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +24,11 @@ public class ProductController {
 	public ResponseEntity<List<Product>> getProducts() {
 		List<Product> prodList = productService.getProducts();
 		return new ResponseEntity<> (prodList, HttpStatus.OK);
+	}
+	
+	@PostMapping("/public/products")
+	public ResponseEntity<Product> postProduct(@RequestBody Product product) {
+		Product savedProduct = productService.createProduct(product);
+		return new ResponseEntity<> (savedProduct, HttpStatus.CREATED);
 	}
 }
