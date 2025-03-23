16575020c2b40b6045f706eab150a83e783c9723
rkyzk <reikoyazaki@yahoo.com>
2025-03-23 14:08:40 +0900

    add Address entity

---
+++ src/main/java/com/restapi/ecommerce/entity/Address.java
@@ -0,0 +1,61 @@
+package com.restapi.ecommerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Table(name = "addresses")
+@Setter
+@Getter
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long addressId;
+
+	@NotBlank
+	@Size(min = 2)
+	private String streetAddress1;
+
+	private String streetAddress2;
+
+	@NotBlank
+	@Size(min = 2)
+	private String city;
+
+	@NotBlank
+	@Size(min = 2)
+	private String province;
+
+	@NotBlank
+	private String countryCode;
+
+	@NotBlank
+	private String postalCode;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	@NotNull
+	private User user;
+
+	public Address(String streetAddress1, String streetAddress2,
+			String city, String province, String countryCode, String postalCode) {
+		this.streetAddress1 = streetAddress1;
+		this.streetAddress2 = streetAddress2;
+		this.city = city;
+		this.province = province;
+		this.countryCode = countryCode;
+		this.postalCode = postalCode;
+	}
+}
--- src/main/java/com/restapi/ecommerce/entity/User.java
+++ src/main/java/com/restapi/ecommerce/entity/User.java
@@ -1,17 +1,30 @@
 package com.restapi.ecommerce.entity;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Data
@@ -49,20 +62,20 @@ public class User {
 		this.password = password;
 	}
 
-//	@Getter
-//	@Setter
-//	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
-//				fetch = FetchType.EAGER)
-//	@JoinTable(
-//			name = "user_role",
-//			joinColumns = @JoinColumn(name = "user_id"),
-//			inverseJoinColumns = @JoinColumn(name = "role_id"))
-//	private Set<Role> roles = new HashSet<Role>();
+	@Getter
+	@Setter
+	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+				fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "user_role",
+			joinColumns = @JoinColumn(name = "user_id"),
+			inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<Role>();
 
-//	@Getter
-//	@Setter
-//	@OneToMany(mappedBy = "user",
-//			cascade = {CascadeType.PERSIST, CascadeType.MERGE},
-//			orphanRemoval = true)
-//	private List<Address> addresses = new ArrayList<>();
+	@Getter
+	@Setter
+	@OneToMany(mappedBy = "user",
+			cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+			orphanRemoval = true)
+	private List<Address> addresses = new ArrayList<>();
 }
