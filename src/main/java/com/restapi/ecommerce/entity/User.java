e135b558975e5a6e7cd3f2e50f1ac15883141485
rkyzk <reikoyazaki@yahoo.com>
2025-03-23 13:50:32 +0900

    add Role entity and AppRole(enum)

--- src/main/java/com/restapi/ecommerce/entity/User.java
+++ src/main/java/com/restapi/ecommerce/entity/User.java
@@ -1,29 +1,17 @@
 package com.restapi.ecommerce.entity;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import com.amazonaws.services.identitymanagement.model.Role;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Data
@@ -61,15 +49,15 @@ public class User {
 		this.password = password;
 	}
 
-	@Getter
-	@Setter
-	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
-				fetch = FetchType.EAGER)
-	@JoinTable(
-			name = "user_role",
-			joinColumns = @JoinColumn(name = "user_id"),
-			inverseJoinColumns = @JoinColumn(name = "role_id"))
-	private Set<Role> roles = new HashSet<Role>();
+//	@Getter
+//	@Setter
+//	@ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+//				fetch = FetchType.EAGER)
+//	@JoinTable(
+//			name = "user_role",
+//			joinColumns = @JoinColumn(name = "user_id"),
+//			inverseJoinColumns = @JoinColumn(name = "role_id"))
+//	private Set<Role> roles = new HashSet<Role>();
 
 //	@Getter
 //	@Setter
