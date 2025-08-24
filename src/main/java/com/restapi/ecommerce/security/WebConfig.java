6cc72db0d3a92e3a8b7df3cc02a5da327e4ff31c
rkyzk <reikoyazaki@yahoo.com>
2025-08-24 11:43:18 +0000

    WebConfigのフロントエンドURL変数をプロパティファイルで設定

--- src/main/java/com/restapi/ecommerce/security/WebConfig.java
+++ src/main/java/com/restapi/ecommerce/security/WebConfig.java
@@ -6,11 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${frontend.URL}")
+	private String frontEndUrl;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(frontEndUrl) // アクセス可のフロントエンドURLを設定
                 .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
